4ffee3173be21dced400c4462faea53f548a99e5
Morteza Morovati <Morteza2m.s@gmai.com>
2020-08-05 11:32:00 +0430

    This project is tutorial project!

---
+++ src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args)  {
+        Thread Account=new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        Account.start();
+    }
+}
